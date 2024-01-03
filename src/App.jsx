6046b0d32a633af43cd42dd4eabd248c8eeeede0
rose <113295030+ros-e2@users.noreply.github.com>
2024-01-03 19:02:38 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import React from 'react';
+import Main from './pages/main/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
